39925cbe150637360a4c1e0a08ccd03d6f3253f0
unknown <wali.azam13@gmail.com>
2024-03-11 21:33:40 +0500

    Working with modules and packages

---
+++ modules&packages/mymodule.py
@@ -0,0 +1,12 @@
+'''
+A module is simply a file containing Python definitions and statements. 
+The file name is the module name with the suffix .py added. Within a module, 
+its name (as a string) is available as the value of the global variable __name__.
+
+'''
+
+# Filename: mymodule.py
+
+def myfunction():
+    print("This is my module and it is designed to add two numbers")
+    print(4 + 5)
